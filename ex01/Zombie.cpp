0d0e7c5088306f8f5245a46c926c33584eda3af5
tndreka <tndreka@student.42heilbronn.de>
2025-06-16 18:54:40 +0200

    ADD: removed the vaibables that i dont need and also updated the makefiel

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/26 18:22:18 by tndreka           #+#    #+#             */
-/*   Updated: 2025/06/14 15:17:25 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/06/16 18:09:56 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,12 @@
 Zombie::Zombie(std::string name)
 {
 	_name = name;
-	// n = 0;
 }
 
 Zombie::~Zombie()
 {
 	std::cout << _name << "is being destroyed ! ! !\n";
 }
-
 
 void	Zombie::announce(void)
 {
