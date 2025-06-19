1d48f8818713c3076d978114dbbe0b2870441a72
tndreka <tndreka@student.42heilbronn.de>
2025-06-20 01:23:52 +0200

    ADD: added the class on header file

--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,7 +6,18 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 19:28:37 by tndreka           #+#    #+#             */
-/*   Updated: 2025/06/16 19:28:38 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/06/20 00:10:38 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+class Weapon
+{
+	private:
+		std::string type;
+	public:
+	Weapon();
+	~Weapon();
+};
