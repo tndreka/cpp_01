a64a09e969b9fa8b1eaf36be04fed156d5a351ee
tndreka <tndreka@student.42heilbronn.de>
2025-06-22 17:51:50 +0200

    ADD: added getter and setter in the header file for getType(Weapon) & setType(NewWeapon). in the .cpp file i implemented the simple function in which the getType is referenced by the type of weapon that we declared. and the settype set the new weapon through the programm.

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -6,9 +6,25 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 19:28:34 by tndreka           #+#    #+#             */
-/*   Updated: 2025/06/18 15:27:11 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/06/22 17:47:27 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Weapon.hpp"
 
+Weapon::Weapon()
+{
+	type = "";
+}
+
+Weapon::~Weapon(){}
+
+const std::string& Weapon::getType()const
+{
+	return type;
+}
+
+void Weapon::setType(std::string newType)
+{
+	type = newType;
+}
--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -6,7 +6,7 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/16 19:28:37 by tndreka           #+#    #+#             */
-/*   Updated: 2025/06/20 00:10:38 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/06/22 17:39:31 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,6 @@ class Weapon
 	public:
 	Weapon();
 	~Weapon();
+	const std::string &getType() const;
+	void setType(std::string);
 };
