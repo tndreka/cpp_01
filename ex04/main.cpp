a2c72f293907c6413452e0fe95a42f3d8752fa0c
tndreka <tndreka@student.42heilbronn.de>
2025-06-23 16:29:59 +0200

    ADD: added the nescessary libraries for opening files and string operations, and also made a check for opening the file successfuly or not

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,17 +6,26 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 15:52:47 by tndreka           #+#    #+#             */
-/*   Updated: 2025/06/23 16:14:02 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/06/23 16:28:02 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
+#include <iostream> //print
+#include <string> // string operations
+#include <fstream> //read files and writefiles
 
 int main(int ac, char *av[])
 {
 	(void)av;
 	if (ac == 4)
 	{
+		std::ifstream inputFile("test.txt");
+		if(inputFile.is_open())
+		{
+			std::cout << "test.txt file was opened \n";
+		}
+		else
+			std::cout << "test.txt file was NOT opened \n";
 		std::cout << "success ! ! !\n";
 	}
 	else
