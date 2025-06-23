675dca0f049e830e43d08fa6e8a526de7941808f
tndreka <tndreka@student.42heilbronn.de>
2025-06-23 16:46:13 +0200

    ADD: added the getline() to get the text from  the file and to print it on stdout

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/23 15:52:47 by tndreka           #+#    #+#             */
-/*   Updated: 2025/06/23 16:28:02 by tndreka          ###   ########.fr       */
+/*   Updated: 2025/06/23 16:45:11 by tndreka          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,25 @@
 int main(int ac, char *av[])
 {
 	(void)av;
+	std::string line;
 	if (ac == 4)
 	{
 		std::ifstream inputFile("test.txt");
 		if(inputFile.is_open())
 		{
 			std::cout << "test.txt file was opened \n";
+			std::string content;
+			while (std::getline(inputFile, line))
+			{
+				content = content + line + "\n";
+			}
+			std::cout << content;// prints the content of the text.txt file after the read
 		}
 		else
-			std::cout << "test.txt file was NOT opened \n";
+		{
+			std::cout << "Error: test.txt file was NOT opened \n";
+			return 1;
+		}
 		std::cout << "success ! ! !\n";
 	}
 	else
