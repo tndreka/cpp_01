b9c82290e0e2fe37b1d3b36cbfd60fd567c5577c
tndreka <tndreka@student.42heilbronn.de>
2025-06-24 20:41:53 +0200

    ADD: added the clas in .hpp and structure it. in .pp i created every private function

---
+++ ex05/Harl.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/24 19:52:36 by tndreka           #+#    #+#             */
+/*   Updated: 2025/06/24 20:40:40 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl()
+{}
+
+Harl::~Harl()
+{}
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!\n";
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put"
+	<< " enough bacon in my burger! If you did, I wouldn’t be asking for more!\n";
+}
+
+void	Harl::warning(void)
+{
+	std::cout << " I think I deserve to have some extra bacon for free. I’ve been coming for"
+			<< "years, whereas you started working here just last month.\n";
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now.\n";
+}
+
+void	Harl::complain(std::string level)
+{
+
+}
---
+++ ex05/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tndreka < tndreka@student.42heilbronn.d    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/24 19:52:39 by tndreka           #+#    #+#             */
+/*   Updated: 2025/06/24 20:06:55 by tndreka          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl
+{
+private:
+	void debug(void);
+	void info(void);
+	void warning(void);
+	void error(void);
+public:
+	Harl();
+	~Harl();
+	void complain(std::string level);
+};
+
+#endif
